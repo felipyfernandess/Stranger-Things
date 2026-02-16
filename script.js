e074a803595b9c286843eb822f7e800d1cb30985
Felipy Fernandes Souza <felipyfernandessouza@gmail.com>
2026-02-15 21:58:41 -0300

    adicionando animações

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-gsap.registerPlugin(ScrollTrigger,ScrollSmoother,SplitText)
+gsap.registerPlugin(ScrollTrigger,ScrollSmoother,SplitText);
+
+ScrollSmoother.create({
+    smooth: 1.5,
+    effects: true
+});
+
+// ANIMAÇÕES HERO
+
+gsap.from('.hero', {
+    opacity: 0,
+    duration: 1.5
+});
+
+gsap.from('picture:nth-child(2)', {
+    y: 60,
+    duration: 1
+});
+
+gsap.from('picture:nth-child(1)', {
+    y: -60,
+    duration: 1
+});
+
+// ANIMAÇÕES CARDS
+
+gsap.from('.card', {
+    opacity: 0,
+    filter: 'blur(10px)',
+    stagger: .3,
+    scrollTrigger: {
+        trigger: '.cards',
+        markers: true,
+        start: '0% 80%',
+        end: '100% 70%',
+        scrub: true
+    }
+});
+
